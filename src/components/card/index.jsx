d41459baedf33c67421652c58d01a6837d6092f4
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2023-10-07 21:04:26 -0300

    added card and fetch

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,15 +1,18 @@
 
-const Card = () => {
+const Card = (data) => {
+  const { price, images, title, category } = data.data;
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
-        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">Electronic</span>
-        <img className="w-full h-full object-cover rounded-lg" src="https://images.pexels.com/photos/815494/pexels-photo-815494.jpeg" alt="headphone" />
+        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
+          {category.name}
+        </span>
+        <img className="w-full h-full object-cover rounded-lg" src={images[0]} alt={title} />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 text-xs text-black">+</div>
       </figure>
       <p className="flex justify-between text-black ">
-        <span className="text-sm font-light px-3 py-1">Headphones</span>
-        <span className="text-lg font-medium px-3">$300</span>
+        <span className="text-xs font-light px-3">{title}</span>
+        <span className="text-lg font-medium px-2">${price}</span>
       </p>
     </div>
   )
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from "react";
 import Layout from "../../components/layout";
 import Card from "../../components/card";
 
 const Home = () => {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    getProduct();
+  }, [])
+  
+  const getProduct = async() =>{
+    try {
+      const data  = await (await fetch('https://api.escuelajs.co/api/v1/products')).json()
+      setItems(data)
+    } catch (error) {
+      console.log('error: ', error)
+    }
+  }
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map(item => <Card key={item.id} data={item}/>)
+        }
+      </div>
     </Layout>
   )
 }
