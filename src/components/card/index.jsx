350ea8383dfd86d49b913d41e9a2c1131afe4e4d
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2023-10-08 04:06:35 -0300

    added order

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -5,10 +5,19 @@ import { PlusCircleIcon } from '@heroicons/react/24/solid'
 const Card = (data) => {
   const context = useContext(ShoppingCartContext)
   const { price, images, title, category } = data.data;
+  const showProduct = (productDetail) => {
+    context.openProductDetail()
+    context.setProductToShow(productDetail)
+  }
+  const addProductToCard = (product) => {
+    context.setCount(context.count + 1)
+    context.setCartProducts([...context.cartProducts, product])
+    console.log('card: ', context.cartProducts)
+  }
   return (
     <div 
       className="bg-white cursor-pointer w-56 h-60 rounded-lg"
-      onClick={() => context.openProductDetail()}
+      onClick={() => showProduct(data.data)}
     >
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
@@ -17,7 +26,7 @@ const Card = (data) => {
         <img className="w-full h-full object-cover rounded-lg" src={images[0]} alt={title} />
         <div 
           className="absolute top-0 right-0 flex justify-center items-center bg-white/75 w-7 h-7 rounded-full m-2 p-1 text-black/40"
-          onClick={() => context.setCount(context.count + 1)}>
+          onClick={() => addProductToCard(data.data)}>
             <PlusCircleIcon className="w-6 h-6"/>
         </div>
       </figure>
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,18 +4,27 @@ export const ShoppingCartContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 export const ShoppingCartProvider = ({children}) => {
+  // Shopping card icmrement
   const [count, setCount] = useState(0);
+  // Detail open and close
   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
-
   const openProductDetail = () => setIsProductDetailOpen(true)
   const closeProductDetail = () => setIsProductDetailOpen(false)
+  // Product detail
+  const [productToShow, setProductToShow] = useState({})
+  // Shop card add products
+  const [cartProducts, setCartProducts] = useState([])
   return (
     <ShoppingCartContext.Provider value={{
       count,
       setCount,
       isProductDetailOpen,
       openProductDetail,
-      closeProductDetail
+      closeProductDetail,
+      productToShow,
+      setProductToShow,
+      cartProducts,
+      setCartProducts
     }}> 
       {children}
     </ShoppingCartContext.Provider>
